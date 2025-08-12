736f7b4b90462a80ad5536de8e89b9342714a962
Odaviespecifico <jak_singer@hotmail.com>
2025-08-12 17:00:45 -0300

    Basic React app

---
+++ linguaskill-mock/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>test</h1>
+      <h2>test 2</h2>
+    </>
+  )
+}
+
+export default App
