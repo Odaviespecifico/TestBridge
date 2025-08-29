535ffdb2543c203a82729dee1cf54b874c9631f7
Odaviespecifico <jak_singer@hotmail.com>
2025-08-29 13:34:51 -0300

    Refactores Linguaskill.JSX to be more modular with utils left aside

--- linguaskill-mock/src/Linguaskill.jsx
+++ linguaskill-mock/src/Linguaskill.jsx
@@ -4,6 +4,8 @@ import { OneCollumnQuestion, TwoCollumnQuestion, OneQuestionMultipleChoice, Drag
 import {AudioAlternative, InlineOpen,DropAlternative,} from "./Alternatives.jsx";
 import {adicionarTentativa} from './supabase.js'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import {Header, Instruction, Introduction, Footer, Loading} from './utils.jsx'
+
 
 export default function Linguaskill() {
   let answers = Object()
@@ -11,6 +13,64 @@ export default function Linguaskill() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   
   const formRef = useRef(null) 
+  const goToNextQuestion = async () => {
+    historicoId.push(questionId)
+    // Verifica se o local Storage não tem o ID da tentativa e verifica o registro de tentativa
+    if (currentQuestion >= 1 && currentQuestion < 2 && localStorage.getItem('id') == null) {
+      if (formRef.current.reportValidity()) {
+        let myform = new FormData(formRef.current)
+        let myformObj = Object.fromEntries(myform.entries())
+        setCurrentQuestion(1.5)
+        let id = await adicionarTentativa(myformObj.studentName,myformObj.teacherName,myformObj.sessionId)
+        if (id.error != null) {
+          console.log('erro')
+        }
+        else {
+          setTimeout(() => {
+            setCurrentQuestion(1.75)
+          }, 500);
+          localStorage.setItem('id',id.data.id)
+          setTimeout(() => {
+            setCurrentQuestion(2)
+          }, 2500);
+        }
+      }
+    }
+    
+    // Caso tenha o ID, pular o registro de tentativa
+    else if (localStorage.getItem('id') != null && currentQuestion == 0) {
+      setCurrentQuestion((prevState) => prevState + 2);
+    }
+
+    // Parse do formulário
+    else if (formRef.current) {
+      let myform = new FormData(formRef.current)
+      let myformObj = Object.fromEntries(myform.entries())
+      myform.entries().forEach((pair) => {
+        localStorage.setItem(pair[0],pair[1])
+      })
+      answers = localStorage
+      setCurrentQuestion((prevState) => prevState + 1);
+    }
+    else {
+      setCurrentQuestion((prevState) => prevState + 1);
+    }
+  };
+
+  const goToPreviousQuestion = () => {
+    setCurrentQuestion((prevState) => prevState - 1);
+    // Atualiza o valor da questão com base
+    setQuestionId(historicoId.at(-2))
+    historicoId.pop()
+
+    // Caso tenha o ID pular a de cadastro de tentativa
+    if (localStorage.getItem('id') != null && currentQuestion == 2) {
+      // Reinicia o contador
+      setQuestionId(1)
+      historicoId = []
+      setCurrentQuestion((prevState) => prevState - 1);
+    }
+  };
 
   function renderQuestions() {
     switch (currentQuestion) {
@@ -186,64 +246,6 @@ export default function Linguaskill() {
       }
   }
 
-  const goToNextQuestion = async () => {
-    historicoId.push(questionId)
-    // Verifica se o local Storage não tem o ID da tentativa e verifica o registro de tentativa
-    if (currentQuestion >= 1 && currentQuestion < 2 && localStorage.getItem('id') == null) {
-      if (formRef.current.reportValidity()) {
-        let myform = new FormData(formRef.current)
-        let myformObj = Object.fromEntries(myform.entries())
-        setCurrentQuestion(1.5)
-        let id = await adicionarTentativa(myformObj.studentName,myformObj.teacherName,myformObj.sessionId)
-        if (id.error != null) {
-          console.log('erro')
-        }
-        else {
-          setTimeout(() => {
-            setCurrentQuestion(1.75)
-          }, 500);
-          localStorage.setItem('id',id.data.id)
-          setTimeout(() => {
-            setCurrentQuestion(2)
-          }, 2500);
-        }
-      }
-    }
-    
-    // Caso tenha o ID, pular o registro de tentativa
-    else if (localStorage.getItem('id') != null && currentQuestion == 0) {
-      setCurrentQuestion((prevState) => prevState + 2);
-    }
-
-    // Parse do formulário
-    else if (formRef.current) {
-      let myform = new FormData(formRef.current)
-      let myformObj = Object.fromEntries(myform.entries())
-      myform.entries().forEach((pair) => {
-        localStorage.setItem(pair[0],pair[1])
-      })
-      answers = localStorage
-      setCurrentQuestion((prevState) => prevState + 1);
-    }
-    else {
-      setCurrentQuestion((prevState) => prevState + 1);
-    }
-  };
-
-  const goToPreviousQuestion = () => {
-    setCurrentQuestion((prevState) => prevState - 1);
-    // Atualiza o valor da questão com base
-    setQuestionId(historicoId.at(-2))
-    historicoId.pop()
-
-    // Caso tenha o ID pular a de cadastro de tentativa
-    if (localStorage.getItem('id') != null && currentQuestion == 2) {
-      // Reinicia o contador
-      setQuestionId(1)
-      historicoId = []
-      setCurrentQuestion((prevState) => prevState - 1);
-    }
-  };
     return (
       <div className="flex items-center flex-col h-full w-screen justify-stretch">
         <Header />
@@ -257,107 +259,9 @@ export default function Linguaskill() {
     );
   }
 
-export function Header() {
-  return <div className="flex w-full h-10 bg-neutral-800"></div>;
-}
 
-export function Instruction({ children }) {
-  if (children) {
-    return (
-      <div className="flex items-center p-7 py-4 h-fit text-lg/5 w-full bg-neutral-700 font-normal text-white">
-        {children}
-      </div>
-    )  
-  }
-  else {
-    <div className="absolute hidden"></div>;
-  }
-}
 
-export function Introduction({title,children}) {
-  return (
-    <div className="flex flex-col gap-10 py-7 max-w-3xl xl:-ml-48 px-6 flex-1">
-      <img
-        src="./Logo-Linguaskill.jpg"
-        alt="Linguaskill Logo"
-        className="h-[65px] w-fit"
-      />
-      <div>
-        <h1 className="text-4xl font-bold mb-7">{title}</h1>
-        <div className="mb-3 text-xl/loose"> 
-        {children}
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export function Footer({ nextQuestion, previousQuestion, currentQuestion }) {
-  const renderBottomButton = () => {
-    if (currentQuestion != 0) {
-      return (
-        <div className="flex gap-4 justify-center items-center">
-          <button
-            type="button"
-            form="formQuestion"
-            onClick={previousQuestion}
-            className="text-gray-100 p-1 font-black text-3xl size-10 border-3 rounded-full"
-          >
-            <img src="/arrow.png" alt="" className="invert rotate-180 size-full hover:opacity-80"/>
-          </button>
-          <button
-            type="button"
-            form="formQuestion"
-            onClick={nextQuestion}
-            className="text-gray-100 p-1 font-medium text-2xl size-10 border-3 rounded-full"
-          >
-            <img src="/arrow.png" alt="" className="invert size-full hover:opacity-80"/>
-          </button>
-        </div>
-      );
-    }
-    else {
-      return (
-        <button
-          type="button"
-          onClick={nextQuestion}
-          className="text-gray-100 font-medium text-2xl hover:text-gray-300"
-        >
-          Start
-        </button>
-      );
-    }
-  };
-
-  return (
-    <div className="flex w-full min-h-14 bg-gray-950 flex-row-reverse px-10 justify-self-end mt-auto z-2">
-      {renderBottomButton()}
-    </div>
-  );
-}
-
-// Small components
-
-export function Loading({status}) {
-  if (status == 'sucess') {
-    return (
-      <div className="flex justify-center items-center h-full">
-        <DotLottieReact className="h-6/12 hue-rotate-250 brightness-160 saturate-200"
-        src="./success.lottie"
-        autoplay/>
-      </div>
-    )}
-      
-  else {
-    return (
-      <div className="flex h-full justify-center items-center">
-        <img src="./loading.svg" alt="Loading icon" className="size-9/12" />
-      </div>
-    )
-  }
-}
-
-// Util function
+// Util functions
 
 let questionId = 1
 let historicoId = []
